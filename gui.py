6fd4840f7661d706c3e32e1f3ba0b61774e7039c
alokekumardas <34475068+alokekumardas@users.noreply.github.com>
2023-03-10 16:30:24 +0530

    Adding Help Button

--- gui.py
+++ gui.py
@@ -9,20 +9,26 @@
 import tkinter
 from tkinter import * 
 import os 
-  
+import webbrowser  
 top = Tk() 
 top.title("HV test in I-Seg") 
   
-top.geometry("400x250")  
+top.geometry("400x250") 
+ 
+def callback():
+        webbrowser.open_new(r"https://github.com/alokekumardas/HV_test_iseg/blob/main/README.md")
 
-def runTest():	
+def runTest():
+	#os.system(r'cd C:\Users\dell\Desktop\HV_test\HV_test_iseg')	
 	#os.system("python3 run_HV_test.py -s %s -e %s -p %s"%(var1,var2,var3))
 	os.system("python3 run_HV_test.py -s %s -e %s -p %s"%(en1.get(),en2.get(),en3.get()))
 
 Vi = tkinter.Label(top, text = "V_start").place(x = 30,y = 50)    
 Vf = tkinter.Label(top, text = "V_end").place(x = 30, y = 90)    
 step = tkinter.Label(top, text = "V_step").place(x = 30, y = 130)  
-  
+
+link =tkinter.Button(top, text="Help", command=callback)
+link.place(x=350, y=10)  
   
 en1=tkinter.Entry(top)
 en1.place(x = 80, y = 50)  
@@ -43,7 +49,6 @@ en3.place(x = 80, y = 130)
 
 
 Developername = tkinter.Label(top, text = "Developed by A. K. Das").place(x = 90,y = 230) 
-
 starttest = tkinter.Button(top, text = "Start Test",activebackground = "pink", activeforeground = "blue",command=runTest)
 starttest.place(x = 30, y = 170)  
 
